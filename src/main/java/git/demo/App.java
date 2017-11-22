e50a4deb04cb37bc70bcc95360b9cd7e9f675f0a
rverm6 <rverma7@sapient.com>
2017-11-22 12:54:51 +0530

    add user functionality completed

--- src/main/java/git/demo/App.java
+++ src/main/java/git/demo/App.java
@@ -8,7 +8,10 @@ public class App {
 	public static void main(final String[] args) {
 		System.out.println("Starting the app!");
 
-		final User user = new User("Rahul", "Verma", "rahul@gmail.com");
-		System.out.println(user);
+		final User user1 = new User("Rahul", "Verma", "rahul@gmail.com");
+		System.out.println(user1);
+
+		final User user2 = new User("Rahul", "Sharma", "sharmaji@gmail.com");
+		System.out.println(user2);
 	}
 }
