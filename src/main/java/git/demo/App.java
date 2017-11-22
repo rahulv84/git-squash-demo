0c10e8e4a7bed472a27f311ad21cbf9844412c29
rverm6 <rverma7@sapient.com>
2017-11-22 12:47:52 +0530

    added user pojo

---
+++ src/main/java/git/demo/App.java
@@ -0,0 +1,14 @@
+package git.demo;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(final String[] args) {
+		System.out.println("Hello World!");
+
+		final User user = new User("Rahul", "Verma", "rahul@gmail.com");
+		System.out.println(user);
+	}
+}
---
+++ src/main/java/git/demo/User.java
@@ -0,0 +1,66 @@
+package git.demo;
+
+/**
+ * Hello world!
+ * 
+ */
+public class User {
+	String firstName;
+	String lastName;
+	String email;
+
+	public User() {
+
+	}
+
+	public User(final String firstName, final String lastName,
+			final String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(final String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(final String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	@Override
+	public boolean equals(final Object arg0) {
+		// TODO Auto-generated method stub
+		return super.equals(arg0);
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return new StringBuilder().append(this.firstName).append(" ")
+				.append(this.lastName).append(" ").append(this.email)
+				.toString();
+	}
+}
